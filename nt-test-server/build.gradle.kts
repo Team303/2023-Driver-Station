c5a6c38c0991c42c78c7c4f6b1ef88406c9c7c49
Wowkster <adyowowk@gmail.com>
2022-12-20 12:26:53 -0500

    style: :art: move around deps in build script

--- nt-test-server/build.gradle.kts
+++ nt-test-server/build.gradle.kts
@@ -14,11 +14,12 @@ repositories {
 dependencies {
     // Add ntcore-java
     implementation ("edu.wpi.first.ntcore:ntcore-java:2023.1.1-beta-7")
+    // Add ntcore-jni for runtime.
+    implementation ("edu.wpi.first.ntcore:ntcore-jni:2023.1.1-beta-7:windowsx86-64")
 
     // Add wpiutil-java
     implementation ("edu.wpi.first.wpiutil:wpiutil-java:2023.1.1-beta-7")
+    // Add wpiutil-jni
     implementation ("edu.wpi.first.wpiutil:wpiutil-jni:2023.1.1-beta-7:windowsx86-64")
 
-    // Add ntcore-jni for runtime.
-    implementation ("edu.wpi.first.ntcore:ntcore-jni:2023.1.1-beta-7:windowsx86-64")
 }
