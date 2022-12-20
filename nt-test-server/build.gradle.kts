91797b010747629eae0375d89967734bae33a50b
Wowkster <adyowowk@gmail.com>
2022-12-20 12:14:03 -0500

    chore: initial code required for java server

---
+++ nt-test-server/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    id("java")
+}
+
+group = "com.team303"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+
+    maven(url = "https://frcmaven.wpi.edu/artifactory/release/")
+}
+
+dependencies {
+    // Add ntcore-java
+    implementation ("edu.wpi.first.ntcore:ntcore-java:2023.1.1-beta-7")
+
+    // Add wpiutil-java
+    implementation ("edu.wpi.first.wpiutil:wpiutil-java:2023.1.1-beta-7")
+    implementation ("edu.wpi.first.wpiutil:wpiutil-jni:2023.1.1-beta-7:windowsx86-64")
+
+    // Add ntcore-jni for runtime.
+    implementation ("edu.wpi.first.ntcore:ntcore-jni:2023.1.1-beta-7:windowsx86-64")
+}
